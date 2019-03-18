b9c9b78e6a3255bced73a4492690619a3e41fb99
Sander Tang <ewwitssander@gmail.com>
2019-03-18 14:21:47 -0700

    initialized react test app
    added scss/css assets
    added header/footer components
    added homepage and products pages
    changed gulpfile

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from 'react-router-dom';
+
+import './Assets/css/default.min.css';
+import Header from './Components/headerComponent/header';
+import Footer from './Components/footerComponent/footer';
+import Homepage from './Components/pages/homepage';
+import Products from './Components/pages/products';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+
+          <Header />
+            <Route exact path='/' component={Homepage} />
+            <Route exact path='/Products' component={Products} />
+          <Footer />
+
+        </div>
+      </Router>
     );
   }
 }
